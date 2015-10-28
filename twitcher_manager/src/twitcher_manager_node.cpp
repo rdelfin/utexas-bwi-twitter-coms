37d30aa457acf8ea939c86221ea1973a59fd8708
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2015-10-27 22:27:36 -0500

    Added skeleton manager node

---
+++ twitcher_manager/src/twitcher_manager_node.cpp
@@ -0,0 +1,15 @@
+/*
+ * Created by Ricardo Delfin Garcia on 10/27/15.
+ *
+ * DESCRIPTION HERE
+ */
+
+#include <ros/ros.h>
+
+int main(int argc, char* argv[])
+{
+    ros::init(argc, argv, "twitcher_manager_node");
+    
+    ros::spin();
+}
+
